202e804a4d43afdce6785338d97505b98002e612
sathya.gimenes <sathya.gimenes@gmail.com>
2022-09-05 20:05:59 -0300

    Separei a controller em duas, uma para cityevent e outra para a eventreservation

--- WebAPI_ProjetoFinal/Controllers/CityEventController.cs
+++ WebAPI_ProjetoFinal/Controllers/CityEventController.cs
@@ -9,14 +9,10 @@ namespace WebAPI_ProjetoFinal.Controllers
     public class CityEventController : ControllerBase
     {
         public ICityEventService _cityEvent;
-        private readonly IEventReservationService _eventReservationService;
-        public CityEventController(ICityEventService cityEvent, IEventReservationService eventReservationService)
+        public CityEventController(ICityEventService cityEvent)
         {
             _cityEvent = cityEvent;
-            _eventReservationService = eventReservationService;
         }
-
-        #region CityEvent
 
         [HttpGet("/CityEvent/Consulta")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -74,67 +70,6 @@ namespace WebAPI_ProjetoFinal.Controllers
             }
             return Ok();
         }
-        #endregion
 
-        #region EventReservation
-
-        [HttpGet("/EventReservation/Search")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<List<EventReservation>> SearchReservations()
-        {
-            return Ok(_eventReservationService.SearchReservations());
-        }
-
-        [HttpGet("/EventReservation/{id}/Search")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<EventReservation> SearchReservation(long id)
-        {
-            var reserva = _eventReservationService.SearchReservation(id);
-            if (reserva == null)
-            {
-                return NotFound();
-            }
-            return Ok(reserva);
-        }
-
-        [HttpPost("/EventReservation/Insert")]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        public IActionResult InsertReservation(EventReservation eventReservation)
-        {
-            if (!_eventReservationService.InsertReservation(eventReservation))
-            {
-                return BadRequest();
-            }
-            return CreatedAtAction(nameof(InsertReservation), eventReservation);
-        }
-
-        [HttpPut("/EventReservation/{id}/Update")]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public IActionResult UpdateReservation(long id, EventReservation reservation)
-        {
-            if (!_eventReservationService.UpdateReservation(id, reservation))
-            {
-                return NotFound();
-            }
-            return NoContent();
-        }
-
-        [HttpDelete("/EventReservation/{id}/Delete")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult DeleteReservation(long id)
-        {
-            if (!_eventReservationService.DeleteReservation(id))
-            {
-                return NotFound();
-            }
-            return Ok();
-        }
-
-        #endregion
     }
 }
---
+++ WebAPI_ProjetoFinal/Controllers/EventReservationController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI_ProjetoFinal.Core.Interfaces;
+using WebAPI_ProjetoFinal.Core.Model;
+
+namespace WebAPI_ProjetoFinal.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EventReservationController : ControllerBase
+    {
+        private readonly IEventReservationService _eventReservationService;
+        public EventReservationController(IEventReservationService eventReservationService)
+        {
+            _eventReservationService = eventReservationService;
+        }
+
+
+        [HttpGet("/EventReservation/Search")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<EventReservation>> SearchReservations()
+        {
+            return Ok(_eventReservationService.SearchReservations());
+        }
+
+        [HttpGet("/EventReservation/{id}/Search")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<EventReservation> SearchReservation(long id)
+        {
+            var reserva = _eventReservationService.SearchReservation(id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+            return Ok(reserva);
+        }
+
+        [HttpPost("/EventReservation/Insert")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult InsertReservation(EventReservation eventReservation)
+        {
+            if (!_eventReservationService.InsertReservation(eventReservation))
+            {
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(InsertReservation), eventReservation);
+        }
+
+        [HttpPut("/EventReservation/{id}/Update")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult UpdateReservation(long id, EventReservation reservation)
+        {
+            if (!_eventReservationService.UpdateReservation(id, reservation))
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("/EventReservation/{id}/Delete")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteReservation(long id)
+        {
+            if (!_eventReservationService.DeleteReservation(id))
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+    }
+}
